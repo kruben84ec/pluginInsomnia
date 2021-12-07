145cccbe11883d4486f89aa4977b683a7253e24b
cmiranda4 <cmiranda@auconsis.com.ec>
2021-12-06 23:30:40 -0500

    plantilla acceos de usuario

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ class apiToken {
      token = null;
      tokenProperty = null;
      urlRegexp = null;
+     templateTags = [{
+        name:"getToken",
+        displayName: "Get Token",
+        args:[{
+            displayName: "Ingrese el URL del Login",
+            description:"Debe ingresar el url del login y tomara el token",
+            type:'strings',
+            defaultValue: 'localhost'
+        },
+        {
+            displayName: "Ingrese el URL del Login",
+            description:"Debe ingresar el url del login y tomara el token",
+            type:'strings',
+            defaultValue: 'localhost'
+        }],
+        async run (context, regexp, property) {
+            tokenProperty = property;
+            urlRegexp = regexp;
+            if (!token) {
+                return "No exite token";
+            } else {
+                return token;
+            }
+        }
+    }];
+
 }
 
 module.exports = apiToken;
--- test/test_index.js
+++ test/test_index.js
@@ -12,8 +12,48 @@ describe('Index de plugin para el app de Insomnia', () => {
              expect(token).to.equal(plugin.token);
              expect(tokenProperty).to.equal(plugin.tokenProperty);
              expect(urlRegexp).to.equal(plugin.urlRegexp);
-
         });
-
     });
+
+    describe('Generar la interface para el uso del plugin',()=>{
+        it('Debe tener templateTags',()=>{
+            let templateTags = [{
+                name:"getToken",
+                displayName: 'Get Token',
+                description: "Get token from API",
+                args: [{
+                    displayName: "Ingrese el URL del Login",
+                    description:"Debe ingresar el url del login y tomara el token",
+                    type:'strings',
+                    defaultValue: 'localhost'
+                },
+                {
+                    displayName: "Ingrese el URL del Login",
+                    description:"Debe ingresar el url del login y tomara el token",
+                    type:'strings',
+                    defaultValue: 'localhost'
+                }
+
+            ],
+            async run (context, regexp, property) {
+                tokenProperty = property;
+                urlRegexp = regexp;
+                if (!token) {
+                    return "No exite token";
+                } else {
+                    return token;
+                }
+            }
+            }];
+
+            let argsPlugin_mock = templateTags[0].args;
+
+            let argsPlugin = plugin.templateTags[0].args;
+            
+            expect(templateTags[0].name).to.equal(plugin.templateTags[0].name);
+            expect(templateTags[0].displayName).to.equal(plugin.templateTags[0].displayName);
+            expect(argsPlugin_mock.length).to.equal(argsPlugin.length);
+
+        });}
+    );
 });
